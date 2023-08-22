5d7fd3f6a277661da7f964dad39a60b008920557
Prashant <thapliyalprashant285@gmail.com>
2023-08-22 14:33:14 -0700

    code added

--- server.js
+++ server.js
@@ -1,12 +1,27 @@
+// create server with the use of express
 const express = require("express");
 const app = express();
 
-app.listen(3000);
+// use bodyparse to  parse data from req body
+const bodyparse = require("body-parser");
 
-app.get("/", (req, res) => {
-  console.log("kaise,ho saare");
-  res.send("ke haal hai");
+// use bodyparse in app for json
+app.use(bodyparse.json());
+
+// start the sever with the hgelp of app.listen
+app.listen(8000, () => {
+  console.log("server started at port 8000");
 });
-app.delete("/demo", (req, res) => {
-  res.send("kaim chod");
+
+// use routing like '/'   ,'/home'
+// use get method for feteching data from  seerver
+app.get("/", (req, res) => {
+  console.log("fetching some data using get method");
+  res.send("hey there, prashant is here");
+});
+
+// use post method for  adding data to server
+app.post("/home", (req, res) => {
+  const { name, price } = req.body;
+  res.send("all set to go");
 });
